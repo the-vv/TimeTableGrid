498eb8822b51b598acab832dde60e0d00c8314a6
Vishnu Vinod <vishnuvinod2772001@gmail.com>
2021-12-27 14:56:39 +0530

    Initial commit

---
+++ capacitor.config.ts
@@ -0,0 +1,10 @@
+import { CapacitorConfig } from '@capacitor/cli';
+
+const config: CapacitorConfig = {
+  appId: 'io.ionic.starter',
+  appName: 'Time Table Grid',
+  webDir: 'www',
+  bundledWebRuntime: false
+};
+
+export default config;
